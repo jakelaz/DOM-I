1a9e4067998285a263d4be97f1e8a4527895d154
jakelaz <jake@openmessage.io>
2020-03-10 23:33:26 -0400

    completed basic stretch goals moving on to stretch assignment

--- js/index.js
+++ js/index.js
@@ -40,3 +40,88 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+
+
+// Update the nav links with text for each link
+let nav = document.querySelector('nav');
+let navItems = document.querySelectorAll('nav > a');
+navItems.forEach(function(value, i) {
+  value.textContent = siteContent["nav"][`nav-item-${i+1}`];
+});
+
+//Append and prepend items to the nav
+let appendedNavItem = document.createElement('a'),
+    prependedNavItem = document.createElement('a'),
+    appenddedNavText = document.createTextNode('Testimonials'),
+    prependedNavText = document.createTextNode('Why DOM?');
+
+  
+nav.prepend(prependedNavItem);
+prependedNavItem.appendChild(prependedNavText);
+
+nav.appendChild(appendedNavItem).appendChild(appenddedNavText);
+
+
+// Turn nav text green
+document.querySelectorAll('nav > a').forEach(function(value, i) {
+  value.style.color = 'green';  
+});
+  
+  
+
+// Update CTA section content 
+let ctaH1 = document.querySelector('.cta h1');
+ctaH1.textContent = siteContent["cta"]["h1"];
+
+let ctaBtn = document.querySelector('.cta button');
+ctaBtn.textContent = siteContent["cta"]["button"];
+
+let ctaImg = document.querySelector('.cta img');
+ctaImg.setAttribute('src', siteContent["cta"]["img-src"])
+
+
+// Update main content
+let middleImg = document.getElementById('middle-img');
+middleImg.setAttribute('src', siteContent["main-content"]["middle-img-src"])
+ 
+let mainContentH4 = document.querySelectorAll('.main-content h4');
+let mainContentp = document.querySelectorAll('.main-content p');
+
+mainContentH4[0].textContent = siteContent["main-content"]["features-h4"];
+mainContentp[0].textContent = siteContent["main-content"]["features-content"];
+
+mainContentH4[1].textContent = siteContent["main-content"]["about-h4"]; 
+mainContentp[1].textContent = siteContent["main-content"]["about-content"];
+
+mainContentH4[2].textContent = siteContent["main-content"]["services-h4"];
+mainContentp[2].textContent = siteContent["main-content"]["services-content"];
+
+mainContentH4[3].textContent = siteContent["main-content"]["product-h4"]; 
+mainContentp[3].textContent = siteContent["main-content"]["product-content"];
+
+mainContentH4[4].textContent = siteContent["main-content"]["vision-h4"]; 
+mainContentp[4].textContent = siteContent["main-content"]["vision-content"];
+
+
+// Update contact content
+
+let contactH4 = document.querySelector('.contact > h4');
+contactH4.textContent = siteContent["contact"]["contact-h4"];
+
+let contactParagraphs = document.querySelectorAll('.contact > p');
+contactParagraphs[0].textContent = siteContent["contact"]["address"];
+contactParagraphs[1].textContent = siteContent["contact"]["phone"];
+contactParagraphs[2].textContent = siteContent["contact"]["email"];
+
+
+// Update footer content
+let footerParagraph = document.querySelector('footer > p');
+footerParagraph.textContent = siteContent["footer"]["copyright"];
+
+
+//Stretch button click
+ctaBtn.addEventListener('click', (event) => {
+  ctaH1.textContent = "Dom is RLY awesome!";
+  console.log(nav.childNodes);
+});
